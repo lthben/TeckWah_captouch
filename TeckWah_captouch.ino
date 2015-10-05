962cd21954d4b7eed7853f1a49640e4ce22d0f7e
Benjamin Low <lthben@gmail.com>
2015-10-05 17:29:11 +0800

    commit

--- TeckWah_captouch.ino
+++ TeckWah_captouch.ino
@@ -1,26 +1,34 @@
 /*
  * Author: Benjamin Low
- * Date: June 2015
- * Description: A single cap touch sensor using the AT42QT1010 IC. Connect to 
- * any electrode. An LED lights up if touch is detected. Do a Google image 
+ * Last updated: 5 Oct 2015
+ * Description: A single cap touch sensor using the AT42QT1010 IC. Connect to
+ * any electrode. An LED lights up if touch is detected. Do a Google image
  * search for AT42QT1010 wiring and logic diagrams.
  */
 
 int ledPin = 4;
 int sensePin = 8;
+boolean is_turned_on;
 
 void setup() {
-        pinMode(ledPin, OUTPUT);
-        pinMode(sensePin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensePin, INPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
   if (digitalRead(sensePin) == HIGH) {
-        digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH);
+    if (!is_turned_on) {
+      is_turned_on = true;
+      Serial.println("captouch_on");
+    }
   } else {
-        digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW);
+    if (is_turned_on) {
+      is_turned_on = false;
+      Serial.println("captouch_off");
+    }
   }
-
 }
